48f13b9eb5c37eba648a12482fd4782fa8b22607
Andrii Tagaiev <atagaiev@taskmaverick.com>
2023-11-15 14:50:12 +0100

    #001 - Added logic to finish the match

--- src/SportRadar.FootballScoreBoard.Tests/UnitTests/MatchTests.cs
+++ src/SportRadar.FootballScoreBoard.Tests/UnitTests/MatchTests.cs
@@ -54,6 +54,34 @@ namespace SportRadar.FootballScoreBoard.Tests.UnitTests
         }
 
         [Fact]
+        public void WhenCreatingNewMatch_Then_ItShouldBePossibleToSetStartTimeOfTheMatch()
+        {
+            // Arrange
+            var homeScore = Faker.RandomNumber.Next(0, 200);
+            var awayScore = Faker.RandomNumber.Next(0, 200);
+
+            var homeTeam = Faker.Company.Name();
+            var awayTeam = Faker.Company.Name();
+            var createdTime = DateTimeOffset.Now;
+
+            // Action
+            var customStartTime = DateTimeOffset.Now.AddDays(Faker.RandomNumber.Next(-1, -200));
+            var sut = new Match(homeTeam, awayTeam, homeScore, awayScore, customStartTime);
+
+            // Assert
+            sut.HomeTeamScore.Should().Be(homeScore);
+            sut.AwayTeamScore.Should().Be(awayScore);
+            sut.HomeTeam.Should().Be(homeTeam);
+            sut.AwayTeam.Should().Be(awayTeam);
+
+            sut.CreatedTime.Should().BeCloseTo(createdTime, TimeSpan.FromSeconds(1));
+            sut.StartTime.Should().Be(customStartTime);
+            sut.EndTime.Should().BeNull();
+            sut.Id.Should().NotBeEmpty();
+
+        }
+
+        [Fact]
         public void WhenCreatingDifferentMatches_Then_IdShouldBeDifferent()
         {
             // Arrange
@@ -149,6 +177,93 @@ namespace SportRadar.FootballScoreBoard.Tests.UnitTests
             updatedMatch.CreatedTime.Should().Be(match.CreatedTime);
             updatedMatch.StartTime.Should().Be(match.StartTime);
             updatedMatch.EndTime.Should().Be(match.EndTime);
+        }
+
+        [Fact]
+        public void WhenFinishMatch_Then_ItShouldReturnNewInstanceOfTheMatch_AndInformationFieldsShouldBeTheSame_AndEndTimeEndTimeShouldBeUpdated()
+        {
+            // Arrange
+            var match = new Match(Faker.Country.Name(), Faker.Country.Name(), Faker.RandomNumber.Next(0, 200), Faker.RandomNumber.Next(0, 200));
+
+            // Action
+            var homeTeamFinalScore = Faker.RandomNumber.Next(0, 200);
+            var awayTeamFinalScore = Faker.RandomNumber.Next(0, 200);
+            var finishedMatch = match.FinishMatch(homeTeamFinalScore, awayTeamFinalScore);
+
+            // Assert
+            finishedMatch.Should().NotBeEquivalentTo(match);
+            finishedMatch.HomeTeamScore.Should().Be(homeTeamFinalScore);
+            finishedMatch.AwayTeamScore.Should().Be(awayTeamFinalScore);
+            finishedMatch.Id.Should().Be(match.Id);
+            finishedMatch.HomeTeam.Should().Be(match.HomeTeam);
+            finishedMatch.AwayTeam.Should().Be(match.AwayTeam);
+            finishedMatch.CreatedTime.Should().Be(match.CreatedTime);
+            finishedMatch.StartTime.Should().Be(match.StartTime);
+            finishedMatch.EndTime.Should().NotBeNull();
+            finishedMatch.EndTime.Should().NotBeNull().And.Subject!.Value.Should().BeCloseTo(DateTimeOffset.Now, TimeSpan.FromSeconds(1));
+        }
+
+        [Fact]
+        public void WhenFinishMatch_AndThereCustomEndDate_AndThereIsCustomFinishDate_Then_ItShouldUpdateTheFinishDate()
+        {
+            // Arrange
+            var customStartDate = DateTimeOffset.Now.AddDays(Faker.RandomNumber.Next(-100, -200));
+            var customEndDate = DateTimeOffset.Now.AddDays(Faker.RandomNumber.Next(-1, -99));
+            var match = new Match(Faker.Country.Name(), Faker.Country.Name(), Faker.RandomNumber.Next(0, 200), Faker.RandomNumber.Next(0, 200), customStartDate);
+
+            // Action
+            var homeTeamFinalScore = Faker.RandomNumber.Next(0, 200);
+            var awayTeamFinalScore = Faker.RandomNumber.Next(0, 200);
+            var finishedMatch = match.FinishMatch(homeTeamFinalScore, awayTeamFinalScore, customEndDate);
+
+            // Assert
+            finishedMatch.Should().NotBeEquivalentTo(match);
+            finishedMatch.HomeTeamScore.Should().Be(homeTeamFinalScore);
+            finishedMatch.AwayTeamScore.Should().Be(awayTeamFinalScore);
+            finishedMatch.Id.Should().Be(match.Id);
+            finishedMatch.HomeTeam.Should().Be(match.HomeTeam);
+            finishedMatch.AwayTeam.Should().Be(match.AwayTeam);
+            finishedMatch.CreatedTime.Should().Be(match.CreatedTime);
+            finishedMatch.StartTime.Should().Be(match.StartTime);
+            finishedMatch.EndTime.Should().NotBeNull();
+            finishedMatch.EndTime.Should().NotBeNull().And.Subject!.Value.Should().Be(customEndDate);
+        }
+
+        [Fact]
+        public void WhenFinishMatch_AndThereCustomEndDate_AndFinishDateLessThanStartDate_Then_ItShouldThrowAnException()
+        {
+            // Arrange
+            var customStartDate = DateTimeOffset.Now.AddDays(Faker.RandomNumber.Next(-100, -200));
+            var customEndDate = DateTimeOffset.Now.AddDays(Faker.RandomNumber.Next(-201, -401));
+            var match = new Match(Faker.Country.Name(), Faker.Country.Name(), Faker.RandomNumber.Next(0, 200), Faker.RandomNumber.Next(0, 200), customStartDate);
+            var homeTeamFinalScore = Faker.RandomNumber.Next(0, 200);
+            var awayTeamFinalScore = Faker.RandomNumber.Next(0, 200);
+
+            // Action
+            Action action = () =>
+            {
+                match.FinishMatch(homeTeamFinalScore, awayTeamFinalScore, customEndDate);
+            };
+
+            // Assert
+            action.Should().Throw<ArgumentException>().WithMessage("End time cannot be less than start time.*");
+        }
+
+        [Theory]
+        [MemberData(nameof(TestNegativeScoreData))]
+        public void WhenFinishMatch_AndScoreIsNegative_Then_ThereShouldBeAnException(int homeTeamScore, int awayTeamScore)
+        {
+            // Arrange
+            var match = new Match(Faker.Country.Name(), Faker.Country.Name(), Faker.RandomNumber.Next(0, 200), Faker.RandomNumber.Next(0, 200));
+
+            // Action
+            Action action = () =>
+            {
+                match.FinishMatch(homeTeamScore, awayTeamScore);
+            };
+
+            // Assert
+            action.Should().Throw<ArgumentException>().WithMessage("Score cannot be less than zero.*");
         }
     }
 }
--- src/SportRadar.FootballScoreBoard/Match.cs
+++ src/SportRadar.FootballScoreBoard/Match.cs
@@ -8,6 +8,11 @@ public class Match
     }
 
     public Match(string homeTeam, string awayTeam, int homeTeamScore, int awayTeamScore)
+        : this(homeTeam, awayTeam, homeTeamScore, awayTeamScore, null)
+    {
+    }
+
+    public Match(string homeTeam, string awayTeam, int homeTeamScore, int awayTeamScore, DateTimeOffset? startTime)
     {
         if (string.IsNullOrEmpty(homeTeam))
             throw new ArgumentException("Value cannot be null or empty.", nameof(homeTeam));
@@ -22,17 +27,9 @@ public class Match
         AwayTeam = awayTeam;
         HomeTeamScore = homeTeamScore;
         AwayTeamScore = awayTeamScore;
-        CreatedTime = StartTime = DateTimeOffset.Now;
+        CreatedTime = DateTimeOffset.Now;
+        StartTime = (DateTimeOffset)(startTime.HasValue ? startTime : CreatedTime);
         EndTime = default;
-    }
-
-    private static void VerifyScore(int homeScore, int awayScore)
-    {
-        if (homeScore < 0)
-            throw new ArgumentException("Score cannot be less than zero.", nameof(homeScore));
-
-        if (awayScore < 0)
-            throw new ArgumentException("Score cannot be less than zero.", nameof(awayScore));
     }
 
     public Match UpdateScore(int homeTeamScore, int awayTeamScore)
@@ -43,6 +40,26 @@ public class Match
             CreatedTime = CreatedTime,
             StartTime = StartTime,
             EndTime = EndTime,
+            Id = Id
+        };
+    }
+
+    public Match FinishMatch(int homeTeamFinalScore, int awayTeamFinalScore)
+    {
+        return FinishMatch(homeTeamFinalScore, awayTeamFinalScore, null);
+    }
+
+    public Match FinishMatch(int homeTeamFinalScore, int awayTeamFinalScore, DateTimeOffset? endTime)
+    {
+        VerifyScore(homeTeamFinalScore, awayTeamFinalScore);
+        if (endTime.HasValue && endTime.Value < StartTime)
+            throw new ArgumentException("End time cannot be less than start time.", nameof(endTime));
+
+        return new Match(HomeTeam, AwayTeam, homeTeamFinalScore, awayTeamFinalScore)
+        {
+            CreatedTime = CreatedTime,
+            StartTime = StartTime,
+            EndTime = endTime == null ? DateTimeOffset.Now : endTime,
             Id = Id
         };
     }
@@ -62,4 +79,13 @@ public class Match
     public DateTimeOffset StartTime { get; private set; }
 
     public DateTimeOffset? EndTime { get; private set; }
+
+    private static void VerifyScore(int homeScore, int awayScore)
+    {
+        if (homeScore < 0)
+            throw new ArgumentException("Score cannot be less than zero.", nameof(homeScore));
+
+        if (awayScore < 0)
+            throw new ArgumentException("Score cannot be less than zero.", nameof(awayScore));
+    }
 }

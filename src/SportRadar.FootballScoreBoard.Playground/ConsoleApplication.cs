5ce7d0479ead8fd4ba9e0894c549d631226bfb44
Andrii Tagaiev <atagaiev@taskmaverick.com>
2023-11-16 14:33:11 +0100

    #001 - Added playground example

--- src/SportRadar.FootballScoreBoard.Playground/ConsoleApplication.cs
+++ src/SportRadar.FootballScoreBoard.Playground/ConsoleApplication.cs
@@ -13,21 +13,22 @@ public class ConsoleApplication
 
     public void Run()
     {
+        var ukraineRussia = new Match("Ukraine", "Russia", 5, 0);
+        _matchService.Create(ukraineRussia);
+        _matchService.Update(ukraineRussia.UpdateScore(6, 0));
+        _matchService.Finish(ukraineRussia.FinishMatch(9, 0));
+
         var mexicoCanada = new Match("Mexico", "Canada", 0, 5);
         var spainBrasil = new Match("Spain", "Brasil", 10, 2);
         var germanyFrance = new Match("Germany", "France", 2, 2);
         var uruguayItaly = new Match("Uruguay", "Italy", 6, 6);
         var argentinaAustralia = new Match("Argentina", "Australia", 3, 1);
-        var ukraineRussia = new Match("Ukraine", "Russia", 5, 0);
         _matchService.Create(mexicoCanada);
         _matchService.Create(spainBrasil);
         _matchService.Create(germanyFrance);
         _matchService.Create(uruguayItaly);
         _matchService.Create(argentinaAustralia);
 
-        _matchService.Create(ukraineRussia);
-        _matchService.Update(ukraineRussia.UpdateScore(6, 0));
-        _matchService.Finish(ukraineRussia.FinishMatch(9, 0));
 
         var summaryReport = _matchReportService.Summary().ToArray();
         foreach (var match in summaryReport)
